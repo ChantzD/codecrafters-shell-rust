a373572ac83d82c48ccc738f1523589d929788bc
Chantz Dalton <chantzdalton31@protonmail.com>
2026-03-17 16:12:38 -0400

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,20 @@
 use std::io::{self, Write};
 
 fn main() {
+    loop {
+        print!("$ ");
+        eval(read());
+    }
+}
+
+fn read() -> String {
     let mut input = String::new();
-    print!("$ ");
     // Need to flush buffer to print $ immediately
     io::stdout().flush().expect("Failed to flush");
     io::stdin().read_line(&mut input).expect("Failed to read line");
-    println!("{}: command not found", input.trim());
+    return input.trim().to_string();
+}
+
+fn eval(input : String){
+    println!("{}: command not found", input);
 }
